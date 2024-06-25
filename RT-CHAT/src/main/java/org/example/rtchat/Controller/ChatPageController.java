cebce178e64bea0129c7bdf99fcec3eb9beb704c
BeratAhmetaj <beratahmetaj02@gmail.com>
2024-06-25 20:37:52 +0200

    Github Actions + Kubernetes

--- RT-CHAT/src/main/java/org/example/rtchat/Controller/ChatPageController.java
+++ RT-CHAT/src/main/java/org/example/rtchat/Controller/ChatPageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ChatPageController {
 
-    @GetMapping("/chat")
+    @GetMapping({"/", "/chat"})
     public String getChatPage(Model model) {
         return "chat"; // Return the name of the view template (e.g., chat.html)
     }
